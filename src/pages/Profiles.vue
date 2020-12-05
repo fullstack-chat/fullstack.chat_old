080cb8b6668d8c357ea7ea37f01b3c0692f342f3
Brian Morrison II <brian@brianmorrison.me>
2020-12-04 20:46:45 -0600

    Style tweaks

--- src/pages/Profiles.vue
+++ src/pages/Profiles.vue
@@ -3,16 +3,19 @@
     <div class="container">
       <div class="col-md-4" v-for="(p, index) in profiles" :key="index">
         <div class="profile-card">
-          <div class="header">
-            <div class="profile-img"><img class="img-fluid" :src="p.imageUrl" /></div>
-            <div class="name">{{ p.name }}</div>
+          <div class="profile-img">
+            <img class="img-fluid" :src="p.imageUrl" />
           </div>
+          <div class="name">{{ p.name.substring(0, 140) }}</div>
           <div class="bio">
             {{ p.bio }}
           </div>
           <div class="links">
-            <div class="link" v-for="(s, index) in p.links" :key="index">
-              blah
+            <div class="link" v-for="(key, index) in p.links" :key="index">
+              <a :href="p.links[key]">
+                {{ key }}
+                <img :src="`/assets/images/socials/${key}.png`" />
+              </a>
             </div>
           </div>
         </div>
@@ -23,6 +26,7 @@
 
 <script>
 import profiles from '@/data/profiles.json'
+import githubLogo from "super-tiny-icons/images/svg/github.svg";
 
 export default {
   data() {
@@ -43,9 +47,24 @@ export default {
   flex-direction: column;
 }
 
+.name {
+  font-weight: bold;
+  font-size: 1.1em;
+  text-align: center;
+}
+
 .profile-img {
-  max-height: 100px;
-  max-width: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 10px;
+  border-radius: 15px;
+}
+
+.profile-img img {
+  max-height: 75px;
+  max-width: 75px;
 }
 
 .bio {
@@ -63,5 +82,15 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  width: 100%;
+}
+
+.link img {
+  max-height: 40px;
+  max-width: 40px;
+}
+
+.link:first-child {
+  border-left: none !important
 }
 </style>
